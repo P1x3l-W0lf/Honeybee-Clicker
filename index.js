f5936880e1ae76f5968d5a5be69774ffb752aeff
P1x3l-W0lf <charr2028@gmail.com>
2026-04-09 10:59:36 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let score = 0;
+let auto_click = 0;
+let click_strength = 1;
+
+function clicked(){
+    score+=click_strength;
+    document.getElementById("num").innerText = score.toString();
+}
+
+function buy(item, price){
+    if (price > score){
+        alert("You don't have enough points for that item.")
+    } else{
+        switch(item){
+            case "auto_clicker":
+                auto_click++;
+                score -= price**auto_click;
+                document.getElementById("p/s").innerText = auto_click.toString();
+                document.getElementById("autoPrice").innerText = (price**(auto_click++)).toString();
+                if (!window.autoClickStarted){
+                    startAutoClicker()
+                }
+                break;
+            case "strength":
+                score -= price**click_strength;
+                click_strength++;
+                document.getElementById("cs").innerText = click_strength.toString();
+                document.getElementById("strengthPrice").innerText = (price**click_strength).toString();
+                break;
+        }
+        document.getElementById("num").innerText = score;
+    }
+}
+
+function startAutoClicker() {
+    if (!window.autoClickStarted) {
+        setInterval(() => {
+            score+=auto_click;
+            document.getElementById("num").innerText = score;
+        }, 1000);
+        window.autoClickStarted = true;
+    }
+}
